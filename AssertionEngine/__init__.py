91421df168876ff354fb5ef1b81a13e6ec2bf998
Tatu Aalto <aalto.tatu@gmail.com>
2021-02-28 00:58:30 +0200

    Lint fix

--- AssertionEngine/__init__.py
+++ AssertionEngine/__init__.py
@@ -1,6 +1,3 @@
-from .assertion_engine import (
-    AssertionOperator,
-    verify_assertion,
-)
+from .assertion_engine import AssertionOperator, verify_assertion
 
 __all__ = ["AssertionOperator", "verify_assertion"]
