a01436a22d3b168d00a6d944ae0b6efd8ef4f823
Tatu Aalto <aalto.tatu@gmail.com>
2021-02-28 01:11:21 +0200

    Lint library

--- atest/TestLibrary.py
+++ atest/TestLibrary.py
@@ -4,8 +4,13 @@ from AssertionEngine import verify_assertion, AssertionOperator
 
 
 class TestLibrary:
-
-    def is_equal(self, value: str, assertion_operator: Optional[AssertionOperator] = None, assertion_expected: Any = None, message: str = None):
+    def is_equal(
+        self,
+        value: str,
+        assertion_operator: Optional[AssertionOperator] = None,
+        assertion_expected: Any = None,
+        message: str = None,
+    ):
         return verify_assertion(
             value, assertion_operator, assertion_expected, "URL", message
         )
