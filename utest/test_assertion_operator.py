f234b4b20b3b851aed41283eb4df4ccb639559e3
Tatu Aalto <aalto.tatu@gmail.com>
2021-12-11 22:17:54 +0200

    Improve test coverage

---
+++ utest/test_assertion_operator.py
@@ -0,0 +1,31 @@
+from approvaltests import verify  # type: ignore
+from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory  # type: ignore
+
+from assertionengine import AssertionOperator
+from assertionengine.assertion_engine import NumericalOperators, SequenceOperators, EvaluationOperators, handlers
+
+
+def test_assertion_operator():
+    results = [f"{item.name}::{item.value}" for item in AssertionOperator]
+    results.insert(0, str(AssertionOperator))
+    verify("\n".join(results))
+
+
+def test_numerical_operators():
+    results = [f"{item}" for item in NumericalOperators]
+    verify("\n".join(results))
+
+
+def test_sequence_operators():
+    results = [f"{item}" for item in SequenceOperators]
+    verify("\n".join(results))
+
+
+def test_evaluation_operators():
+    results = [f"{item}" for item in EvaluationOperators]
+    verify("\n".join(results))
+
+
+def test_handlers():
+    results = [f"{item}::{str(handlers[item]).split(' ')[4:]}" for item in handlers]
+    verify("\n".join(results))
