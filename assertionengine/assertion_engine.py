5e4da2b99e4bbf53a8be3faf3e33214d660a8306
Tatu Aalto <aalto.tatu@gmail.com>
2023-01-07 00:55:47 +0200

    Set version

--- assertionengine/assertion_engine.py
+++ assertionengine/assertion_engine.py
@@ -21,7 +21,7 @@ from robot.libraries.BuiltIn import BuiltIn  # type: ignore
 
 from .type_converter import is_truthy, type_converter
 
-__version__ = "0.6.1"
+__version__ = "1.0.0"
 
 AssertionOperator = Enum(
     "AssertionOperator",
